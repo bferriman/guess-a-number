90f54da685483fd83da64ac4887cf3cee7ea72fb
Ben F <56423163+bferriman@users.noreply.github.com>
2021-03-29 21:41:19 -0600

    added comments showing method of specifying remote sourced image and fadeDuration

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -10,8 +10,10 @@ const GameOverScreen = props => {
       <View style={styles.imageContainer}>
         <Image 
           source={require("../assets/images/success.png")} 
+          // source={{uri: "https://images.unsplash.com/photo-1528819622765-d6bcf132f793?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"}}
           style={styles.image}
           resizeMode="cover"
+          // fadeDuration={500}  //specify num of ms over which image fades in on first load
         />
       </View>
       <BodyText>Number of rounds: {props.numRounds}</BodyText>
