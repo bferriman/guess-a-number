e229d57658c98dd99a959997303924577852da93
Ben F <56423163+bferriman@users.noreply.github.com>
2021-03-31 16:59:22 -0600

    added additional styling for list

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -22,7 +22,7 @@ const getRandomBetween = (min, max, exclude) => {
 const renderListItem = (value, numOfRound) => {
   return (
     <View key={value} style={styles.listItem}>
-      <BodyText>#{numOfRound}: </BodyText>
+      <BodyText>rd {numOfRound}</BodyText>
       <BodyText>{value}</BodyText>
     </View>
   );
@@ -71,8 +71,8 @@ const GameScreen = props => {
           <Ionicons name="caret-up" size={24} color="white" />
         </MainButton>
       </Card>
-      <View style={styles.list}>
-        <ScrollView>
+      <View style={styles.listContainer}>
+        <ScrollView contentContainerStyle={styles.list}>
           {pastGuesses.map((guess, index) => renderListItem(guess, (pastGuesses.length - index)))}
         </ScrollView>
       </View>
@@ -93,9 +93,14 @@ const styles = StyleSheet.create({
     width: 400,
     maxWidth: "90%"
   },
-  list: {
+  listContainer: {
     flex: 1,
     width: "80%"
+  },
+  list: {
+    flexGrow: 1,
+    alignItems: "center",
+    justifyContent: "flex-end"
   },
   listItem: {
     borderColor: "#ccc",
@@ -104,7 +109,8 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     backgroundColor: "white",
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    width: "72%"
   }
 });
 
