0a44f1c72b8539910a9b6eef36d5b8bcdd2ed1d1
Ben F <56423163+bferriman@users.noreply.github.com>
2021-04-02 16:22:14 -0600

    added responsive styling to GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, Dimensions, ScrollView } from "react-native";
 
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
@@ -8,24 +8,26 @@ import MainButton from "../components/MainButton";
 
 const GameOverScreen = props => {
   return (
-    <View style={styles.screen}>
-      <TitleText>GAME OVER</TitleText>
-      <View style={styles.imageContainer}>
-        <Image 
-          source={require("../assets/images/success.png")} 
-          // source={{uri: "https://images.unsplash.com/photo-1528819622765-d6bcf132f793?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"}}
-          style={styles.image}
-          resizeMode="cover"
-          // fadeDuration={500}  //specify num of ms over which image fades in on first load
-        />
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>GAME OVER</TitleText>
+        <View style={styles.imageContainer}>
+          <Image 
+            source={require("../assets/images/success.png")} 
+            // source={{uri: "https://images.unsplash.com/photo-1528819622765-d6bcf132f793?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"}}
+            style={styles.image}
+            resizeMode="cover"
+            // fadeDuration={500}  //specify num of ms over which image fades in on first load
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone needed <Text style={styles.highlight}>{props.numRounds}</Text> rounds to guess the number <Text style={styles.highlight}>{props.answer}</Text>.
+          </BodyText>
+        </View>
+        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
       </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your phone needed <Text style={styles.highlight}>{props.numRounds}</Text> rounds to guess the number <Text style={styles.highlight}>{props.answer}</Text>.
-        </BodyText>
-      </View>
-      <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -36,13 +38,13 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: Dimensions.get("window").width * 0.7,
+    height: Dimensions.get("window").width * 0.7,
+    borderRadius: Dimensions.get("window").width * 0.7 / 2,
     borderWidth: 3,
     borderColor: "black",
     overflow: "hidden",
-    marginVertical: 30
+    marginVertical: Dimensions.get("window").height / 30
   },
   image: {
     width: "100%",
@@ -50,11 +52,11 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     marginHorizontal: 50,
-    marginVertical: 15
+    marginVertical: Dimensions.get("window").height / 60
   },
   resultText: {
     textAlign: "center",
-    fontSize: 20
+    fontSize: Dimensions.get("window").height < 1000 ? 16 : 20
   },
   highlight: {
     color: Colors.accent,
